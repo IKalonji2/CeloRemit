dc5727ed64f7c66b071f364cfe66e2fbc8e458c7
IKalonji2 <Issa@bbd.co.za>
2022-06-09 21:28:22 +0200

    Fixed issue with the backend url

--- MobileDapp/src/environments/environment.prod.ts
+++ MobileDapp/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  urlLocal: "https://celo-remit.herokuapp.com/",
-  urlDeployed: "https://celo-remit.herokuapp.com/",
+  urlLocal: "https://celo-remit.herokuapp.com",
+  urlDeployed: "https://celo-remit.herokuapp.com",
   API_KEY: "3d59cd80-9957-4423-8d5e-974bc98af2c0_100",
   web3auth: ""
 }
