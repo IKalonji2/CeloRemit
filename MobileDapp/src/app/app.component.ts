62e038536fe0e79b7f59c9082d52a9ccd74ebc7f
IKalonji2 <Issa@bbd.co.za>
2022-09-21 19:06:29 +0200

    Update

--- MobileDapp/src/app/app.component.ts
+++ MobileDapp/src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChange
 import { AlertController, LoadingController } from '@ionic/angular';
 import { ApiServiceService } from '../services/api-service.service';
 
-import { ADAPTER_EVENTS } from "@web3auth/base";
-import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
 import { Web3Auth } from "@web3auth/web3auth";
 
 import { CHAIN_CONFIG, CHAIN_CONFIG_TYPE } from "./../config/chains";
@@ -13,7 +11,12 @@ import { getWalletProvider, IWalletProvider } from "./../services/wallet-provide
 
 import { UserServiceService } from "src/services/user-service.service";
 
-const clientId = "BD-o_TdBHjHOuKoRHvkouRcFDB00aToIftvdiET5nu4kcdrx0C-_yZP668ojVA2OZp03wGDajoKTuHMz-R4Uq2s";
+import UAuth from '@uauth/js'
+
+const uauth = new UAuth({
+  clientID: "827c788a-979f-4949-88cf-2d653ad16326",
+  redirectUri: "https://celo-remit.vercel.app/home"
+});
 
 @Component({
   selector: "app-root",
@@ -22,19 +25,11 @@ const clientId = "BD-o_TdBHjHOuKoRHvkouRcFDB00aToIftvdiET5nu4kcdrx0C-_yZP668ojVA
 })
 export class AppComponent implements OnInit{
 
-  chain: CHAIN_CONFIG_TYPE = "mainnet";
-
-  network: WEB3AUTH_NETWORK_TYPE = "cyan";
-
   isLoggedIn!: boolean;
 
   domain: any = "none@none.none";
 
-  web3auth: Web3Auth | null = null;
-
   isModalLoaded = false;
-
-  provider: IWalletProvider | null = null;
 
   user: string = ""
 
@@ -43,70 +38,11 @@ export class AppComponent implements OnInit{
     private userService: UserServiceService,
     private alertController: AlertController
     ) {
-
-      // this.web3auth = new Web3Auth({
-      //   clientId,
-      //   chainConfig: CHAIN_CONFIG[this.chain],
-      // });
       this.isLoggedIn = userService.getUserLoginStatus()
       this.initAppToBackend()
     }
     
-  ngOnInit(): void {
-
-    // const subscribeAuthEvents = (web3auth: Web3Auth) => {
-    //   web3auth.on(ADAPTER_EVENTS.CONNECTED, (data) => {
-    //     console.log("Yeah!, you are successfully logged in", data);
-    //     this.setLoginStatus(true);
-    //   });
-
-    //   web3auth.on(ADAPTER_EVENTS.CONNECTING, () => {
-    //     console.log("connecting");
-    //   });
-
-    //   web3auth.on(ADAPTER_EVENTS.DISCONNECTED, () => {
-    //     console.log("disconnected");
-    //     this.setLoginStatus(false);
-    //   });
-
-    //   web3auth.on(ADAPTER_EVENTS.ERRORED, (error) => {
-    //     console.log("some error or user have cancelled login request", error);
-    //   });
-    // };
-
-  //   const initializeModal = async () => {
-  //     console.log("INIT MODAL");
-  //     this.web3auth = new Web3Auth({
-  //       clientId,
-  //       chainConfig: CHAIN_CONFIG[this.chain],
-  //     });
-  //     const adapter = new OpenloginAdapter({ adapterSettings: { network: this.network, clientId } });
-  //     this.web3auth.configureAdapter(adapter);
-
-  //     subscribeAuthEvents(this.web3auth);
-  //     await this.web3auth.initModal();
-  //     this.isModalLoaded = true;
-
-  //     if (this.isLoggedIn && !this.provider) {
-  //       const web3authProvider = await this.web3auth.connect();
-  //       if (web3authProvider) this.provider = getWalletProvider(this.chain, web3authProvider, this.uiConsole);
-  //     }
-  //   };
-  //   initializeModal();
-  // }
-
-  // selectChain(chain: string) {
-  //   this.chain = chain as CHAIN_CONFIG_TYPE;
-  // }
-
-  // selectNetwork(network: string) {
-  //   this.network = network as WEB3AUTH_NETWORK_TYPE;
-  // }
-
-  // setLoginStatus(status: boolean) {
-  //   this.isLoggedIn = status;
-  //   this.setLoginState(status)
-  }
+  ngOnInit(): void {}
 
   initAppToBackend(){
     console.log("before init")
@@ -179,80 +115,16 @@ export class AppComponent implements OnInit{
     }
     this.setAccount(this.user)
   }
-  // async loginWeb3Auth() {
-  //   console.log("LOGGING IN");
-  //   this.setLoginState(true)
-  //   if (!this.web3auth) {
-  //     console.log("Web3auth is not initialized");
-  //     return;
-  //   }
-  //   const web3authProvider = await this.web3auth.connect();
-  //   if (web3authProvider){
-  //     this.provider = getWalletProvider(this.chain, web3authProvider, this.uiConsole);
-  //     console.log(this.provider)
-  //     this.getUserInfo();
-  //   } 
-  //   this.getUserInfo()
-  // }
 
-  // async logout() {
-  //   console.log("LOGGING OUT");
-  //   if (!this.web3auth) {
-  //     console.log("Web3auth is not initialized");
-  //     return;
-  //   }
-  //   await this.web3auth.logout();
-  //   this.provider = null;
-  //   this.isLoggedIn = false;
-  // }
-
-  // async getUserInfo() {
-  //   console.log("GETTING USER INFO");
-  //   if (!this.web3auth) {
-  //     console.log("Web3auth is not initialized");
-  //     return;
-  //   }
-  //   const userInfo = await this.web3auth.getUserInfo();
-  //   console.log(userInfo)
-  //   let user = userInfo.email;
-  //   console.log(this.domain)
-  //   this.setAccount(user);
-  //   this.uiConsole("User Info", userInfo);
-  // }
-
-  // async getBalance() {
-  //   console.log("GETTING ACCOUNT BALANCE");
-  //   if (!this.provider) {
-  //     this.uiConsole("provider is not initialized");
-  //     return;
-  //   }
-  //   this.provider.getBalance();
-  // }
-
-  // async getAccount() {
-  //   console.log("GETTING ACCOUNT");
-  //   if (!this.provider) {
-  //     this.uiConsole("provider is not initialized");
-  //     return;
-  //   }
-  //   this.provider.getAccounts();
-  // }
-
-  // async signMessage() {
-  //   console.log("SIGNING MESSAGE");
-  //   if (!this.provider) {
-  //     this.uiConsole("provider is not initialized");
-  //     return;
-  //   }
-  //   this.provider.signMessage();
-  // }
-
-  // uiConsole(...args: unknown[]): void {
-  //   const el = document.querySelector("#console-ui>p");
-  //   if (el) {
-  //     el.innerHTML = JSON.stringify(args || {}, null, 2);
-  //   }
-  // }
-
+  async loginWithUnstoppable(){
+    try{
+      const authorization = await uauth.loginWithPopup()
+      console.log(authorization)
+      this.setAccount(authorization.idToken.sub)
+    }catch(error){
+      console.log(error);
+    }
+    
+  }
 
 }
